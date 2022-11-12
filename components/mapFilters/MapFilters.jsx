d3ae918a742c4ce91ac62595719757ff50902878
Luis Melgarejo <ti15@emilima.com.pe>
2022-11-12 18:25:11 -0500

    feat: creando componentes base de MapFilters

--- components/mapFilters/MapFilters.jsx
+++ components/mapFilters/MapFilters.jsx
@@ -1,12 +1,36 @@
 "use client"
 
 import styled from "styled-components";
-import { ContainerFilters } from "./styled";
+import Logo from "../logo/Logo";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { COLORS } from "../../assets/styles";
+import { ContainerFilters, ContainerFilter, ContainerSearch } from "./styled";
+
+const Search = () => {
+  return (
+    <ContainerSearch>
+      a
+    </ContainerSearch>
+  )
+}
+
+const Filters = () => {
+  return (
+    <ContainerFilter>
+      <IconButton color={COLORS.WHITE} aria-label="filter">
+        <MenuIcon htmlColor={COLORS.WHITE} color={COLORS.WHITE} />
+      </IconButton>
+    </ContainerFilter>
+  )
+}
 
 const MapFilters = () => {
   return (
     <ContainerFilters>
-      aasdas
+      <Logo />
+      <Search />
+      <Filters />
     </ContainerFilters>
   )
 }
