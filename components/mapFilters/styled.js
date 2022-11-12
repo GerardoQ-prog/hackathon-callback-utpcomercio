114b29851a8ad986eb2bfa043ece2c5ac3b91184
Luis Melgarejo <ti15@emilima.com.pe>
2022-11-12 18:24:11 -0500

    feat: modificando estilos de MapFilters

--- components/mapFilters/styled.js
+++ components/mapFilters/styled.js
@@ -1,10 +1,25 @@
 import styled from "styled-components";
+import { COMPONENT_SIZES } from "../../assets/styles";
 
 export const ContainerFilters = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 65px;
+  height: ${COMPONENT_SIZES.MAP_FILTERS};
   z-index: 100;
+  background: rgba(0,0,0,.1);
+  padding: 10px 15px;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  align-items: center;
+`
+
+export const ContainerSearch = styled.div`
+  max-width: 500px;
+`
+
+export const ContainerFilter = styled.div`
+  
 `
