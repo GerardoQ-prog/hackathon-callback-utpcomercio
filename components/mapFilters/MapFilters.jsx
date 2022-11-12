3d4948ebc4b3c4cdcea3953c4b357c2020081249
Luis Melgarejo <ti15@emilima.com.pe>
2022-11-12 17:16:15 -0500

    feat: creando componente MapFilters

---
+++ components/mapFilters/MapFilters.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import styled from "styled-components";
+import { ContainerFilters } from "./styled";
+
+const MapFilters = () => {
+  return (
+    <ContainerFilters>
+      aasdas
+    </ContainerFilters>
+  )
+}
+
+export default MapFilters
