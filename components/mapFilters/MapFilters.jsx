472979fae3d6938fbb816458371ed50280c179c6
Luis Melgarejo <ti15@emilima.com.pe>
2022-11-12 19:29:46 -0500

    feat: añadiendo filtros base para SidebarFilters

--- components/mapFilters/MapFilters.jsx
+++ components/mapFilters/MapFilters.jsx
@@ -1,26 +1,56 @@
 "use client"
-
+import * as React from 'react';
 import styled from "styled-components";
 import Logo from "../logo/Logo";
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton, Autocomplete, TextField, RadioGroup, FormControlLabel, Radio  } from '@mui/material';
+import { Menu  as MenuIcon } from '@mui/icons-material';
 import { COLORS } from "../../assets/styles";
-import { ContainerFilters, ContainerFilter, ContainerSearch } from "./styled";
+import { ContainerFilters, ContainerFilter, ContainerSearch, SidebarContainer } from "./styled";
 
 const Search = () => {
   return (
     <ContainerSearch>
-      a
+      <Autocomplete
+        disablePortal
+        id="combo-box-places"
+        options={[]}
+        sx={{ width: '100%' }}
+        renderInput={(params) => <TextField {...params} label="Busca un lugar" />}
+      />
     </ContainerSearch>
   )
 }
 
+const RadioTypes = () => {
+  return (
+    <RadioGroup
+        defaultValue="alerts"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="alerts" control={<Radio />} label="Mostrar alertas" />
+        <FormControlLabel value="works" control={<Radio />} label="Mostrar Obras" />
+    </RadioGroup>
+  )
+}
+
+const SidebarFilters = ({ ...props }) => {
+  return (
+    <SidebarContainer {...props}>
+      <Search />
+      <RadioTypes />
+    </SidebarContainer> 
+  )
+}
+
 const Filters = () => {
+  const [ menu, toggleMenu ] = React.useState(false)
+
   return (
     <ContainerFilter>
-      <IconButton color={COLORS.WHITE} aria-label="filter">
-        <MenuIcon htmlColor={COLORS.WHITE} color={COLORS.WHITE} />
+      <IconButton onClick={() => toggleMenu(!menu) }>
+        <MenuIcon htmlColor={COLORS.WHITE} />
       </IconButton>
+      <SidebarFilters className={ menu && 'open' } />
     </ContainerFilter>
   )
 }
@@ -29,7 +59,6 @@ const MapFilters = () => {
   return (
     <ContainerFilters>
       <Logo />
-      <Search />
       <Filters />
     </ContainerFilters>
   )
