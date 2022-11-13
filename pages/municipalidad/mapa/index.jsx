f54223296b6a07b54d8262382e02f570c2c30380
Luis Melgarejo <ti15@emilima.com.pe>
2022-11-13 15:37:15 -0500

    feat: añadiendo mapa a la vista de la municipalidad/mapa

--- pages/municipalidad/mapa/index.jsx
+++ pages/municipalidad/mapa/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import MuniLayout from "../../../layouts/MuniLayout";
+import Mapbox from "../../../components/map/Mapbox";
 
-const MunicipalityMap = () => {
-  return <div>MunicipalidadMap</div>;
+
+const MunicipalityMap = ({ markers = [] }) => {
+
+  return (
+    <MuniLayout size="sm">
+      <Mapbox markers={markers} />
+    </MuniLayout>
+  )
 };
 
 export default MunicipalityMap;
