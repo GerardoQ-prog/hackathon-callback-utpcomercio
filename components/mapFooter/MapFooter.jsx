de9ee3ddc3e3de7dd50bf1415a1291daf516abaf
GerardoQ-prog <gerardoquispe65@gmail.com>
2022-11-13 10:48:19 -0500

    advance

--- components/mapFooter/MapFooter.jsx
+++ components/mapFooter/MapFooter.jsx
@@ -1,18 +1,23 @@
-import { ContainerFooter } from "./styled"
-import { Button,  } from "@mui/material"
-import { Add as AddIcon } from '@mui/icons-material';
+import { ContainerFooter } from "./styled";
+import { Button } from "@mui/material";
+import { Add as AddIcon } from "@mui/icons-material";
 import { COLORS } from "../../assets/styles";
 
 const MapFooter = () => {
   return (
     <>
       <ContainerFooter>
-        <Button style={{ background: COLORS.RED }} variant="contained" disableElevation startIcon={<AddIcon />}>
+        <Button
+          style={{ background: COLORS.RED }}
+          variant="contained"
+          disableElevation
+          startIcon={<AddIcon />}
+        >
           Reportar problema
         </Button>
       </ContainerFooter>
     </>
-  )
-}
+  );
+};
 
-export default MapFooter
+export default MapFooter;
