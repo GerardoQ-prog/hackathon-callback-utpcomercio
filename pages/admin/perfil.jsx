6a49fdf4b0fb480f13dd8282b269ab14f666f5fe
Luis Melgarejo <ti15@emilima.com.pe>
2022-11-13 14:06:52 -0500

    feat: creando vista MuniProfile.jsx

---
+++ pages/admin/perfil.jsx
@@ -0,0 +1,12 @@
+import AdminLayout from "../../layouts/AdminLayout"
+
+
+const MuniProfile = () => {
+  return (
+    <AdminLayout>
+      a
+    </AdminLayout>
+  )
+}
+
+export default MuniProfile
